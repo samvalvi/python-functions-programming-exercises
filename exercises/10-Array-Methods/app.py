f6cb5d073f57ce474008a69c715cc82956d20f9e
samvalvi <samuelvalerin@gmail.com>
2021-03-23 18:19:20 +0000

    final commit

--- exercises/10-Array-Methods/app.py
+++ exercises/10-Array-Methods/app.py
@@ -1,6 +1,6 @@
 names = ['John', 'Kenny', 'Tom', 'Bob', 'Dilan']
-## CREATE YOUR FUNCTION HERE
-def sort_names(list):
-    return list.sort()
+# CREATE YOUR FUNCTION HERE
+def sort_names(value):
+    return sorted(value)
 
 print(sort_names(names))
